45521812d5c0b395ca271bbe9d15a7aa4f4a38c2
MonsieurPetion <lenzpetion@gmail.com>
2017-06-04 01:30:31 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import {Component, ViewChild} from "@angular/core";
+import {Platform} from "ionic-angular";
+import {StatusBar, Splashscreen} from "ionic-native";
+import {LoginPage} from "../pages/login/login";
+
+// import pages
+// end import pages
+
+@Component({
+  templateUrl: 'app.html',
+  queries: {
+    nav: new ViewChild('content')
+  }
+})
+export class MyApp {
+
+  public rootPage: any;
+
+  public nav: any;
+
+  constructor(public platform: Platform) {
+    this.rootPage = LoginPage;
+
+    // show splash screen
+    Splashscreen.show();
+
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
+  // hide splash screen
+  hideSplashScreen() {
+    if (Splashscreen) {
+      setTimeout(() => {
+        Splashscreen.hide();
+      }, 100);
+    }
+  }
+}
+
+
