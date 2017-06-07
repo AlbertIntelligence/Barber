f7b0045cf93d16412504a82833d75d9a49790261
kodeu <koueni-anael.deumeni@polymtl.ca>
2017-06-07 02:28:46 -0400

    Addition of user profile page.

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -1,7 +1,9 @@
 import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+import {TicketConfirmationPage} from "../ticket-confirmation/ticket-confirmation";
 
 /*
- Generated class for the LoginPage page.
+ Generated class for the UserProfile page.
 
  See http://ionicframework.com/docs/v2/components/#navigation for more info on
  Ionic pages and navigation.
@@ -11,7 +13,11 @@ import {Component} from "@angular/core";
   templateUrl: 'user-profile.html'
 })
 export class UserProfilePage {
-  // list of hotels
+  constructor(public nav: NavController) {
+  }
 
-
+  // Open ticket confirmation view page
+  getTicketConfirmation() {
+    this.nav.push(TicketConfirmationPage);
+  }
 }
