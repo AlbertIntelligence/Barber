421c9f11999d1993b2f9db4e7bb9c72efd7bc4de
kodeu <koueni-anael.deumeni@polymtl.ca>
2017-06-06 22:14:21 -0400

    User profile pages updated.

---
+++ src/pages/user-profile/user-profile.ts
@@ -0,0 +1,17 @@
+import {Component} from "@angular/core";
+
+/*
+ Generated class for the LoginPage page.
+
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
+@Component({
+  selector: 'user-profile',
+  templateUrl: 'user-profile.html'
+})
+export class UserProfilePage {
+  // list of hotels
+
+
+}
