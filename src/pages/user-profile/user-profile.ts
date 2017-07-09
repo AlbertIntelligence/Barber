318c260d67dcfee2e641f03d00c1ab04d30b34b4
DESKTOP-5AVA9EH\Boss <lenzpetion@gmail.com>
2017-07-08 23:27:43 -0700

    Datascture LinkedList added

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -63,7 +63,6 @@ export class UserProfilePage {
     dbRefObject.set({currentPosition:this.ticketObject.currentPosition + 1 });
   }
 
-  //------------------------------------------THIS IS THE Algorithm  SECTION----------------------------------------------//
 
 
   //------------------------------------------THIS IS THE OBJECT  SECTION----------------------------------------------//
@@ -107,5 +106,119 @@ export class UserProfilePage {
     $('#ticketPosition').hide();
   }
 
+}
+
+//------------------------------------------THIS IS THE Algorithm  SECTION----------------------------------------------//
+
+class LinkedListItem {
+  value: any;
+  next: LinkedListItem;
+
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  private head: LinkedListItem;
+  constructor(item: LinkedListItem) {
+    this.head = item;
+  }
+
+  // Adds the element at a specific position inside the linked list
+  insert(val, previousItem: LinkedListItem) {
+    let newItem: LinkedListItem = new LinkedListItem(val);
+    let currentItem: LinkedListItem = this.head;
+
+    if (!currentItem) {
+      this.head = newItem;
+    } else {
+      while (true) {
+        if (currentItem === previousItem) {
+          let tempNextItem = previousItem.next;
+          currentItem.next = newItem;
+          newItem.next = tempNextItem;
+          break;
+        } else {
+          currentItem = currentItem.next;
+        }
+      }
+    }
+  }
+
+  // Adds the element at the end of the linked list
+  append(val) {
+    let currentItem: LinkedListItem = this.head;
+    let newItem = new LinkedListItem(val);
+
+    if (!currentItem) {
+      this.head = newItem;
+    } else {
+      while (true) {
+        if (currentItem.next) {
+          currentItem = currentItem.next;
+        } else {
+          currentItem.next = newItem;
+          break;
+        }
+      }
+    }
+  }
+
+  // Add the element at the beginning of the linked list
+  prepend(val) {
+    let newItem = new LinkedListItem(val);
+    let oldHead = this.head;
+
+    this.head = newItem;
+    newItem.next = oldHead;
+  }
+
+  delete(val) {
+    var currentItem = this.head;
+
+    if (!currentItem) {
+      return;
+    }
+
+    if (currentItem.value === val) {
+      this.head = currentItem.next;
+    } else {
+      var previous = null;
+
+      while (true) {
+        if (currentItem.value === val) {
+          if (currentItem.next) { // special case for last element
+            previous.next = currentItem.next;
+          } else {
+            previous.next = null;
+          }
+          currentItem = null; // avoid memory leaks
+          break;
+        } else {
+          previous = currentItem;
+          currentItem = currentItem.next;
+        }
+      }
+    }
+  }
+
+  showInArray() {
+    let arr = [];
+    let currentItem = this.head;
+
+    while (true) {
+      arr.push(currentItem.value);
+
+      if (currentItem.next) {
+        currentItem = currentItem.next;
+      } else {
+        break;
+      }
+    }
+
+    return arr;
+  }
 }
 
