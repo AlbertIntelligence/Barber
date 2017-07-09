7d6caea6863b2375e6ace577909dd500919c2960
DESKTOP-5AVA9EH\Boss <lenzpetion@gmail.com>
2017-07-08 23:14:29 -0700

    Ticket Div disapear after 5 second.

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -3,13 +3,9 @@ import {NavController} from "ionic-angular";
 import {TicketConfirmationPage} from "../ticket-confirmation/ticket-confirmation";
 import firebase from 'firebase';
 import {Alert} from "../alert/alert";
-//import { AlertController } from 'ionic-angular';
-/*
- Generated class for the UserProfile page.
+import * as $ from 'jquery';
 
- See http://ionicframework.com/docs/v2/components/#navigation for more info on
- Ionic pages and navigation.
- */
+
 @Component({
   selector: 'user-profile',
   templateUrl: 'user-profile.html'
@@ -22,10 +18,62 @@ export class UserProfilePage {
 
 
   constructor(public nav?: NavController, private newAlert?: Alert) {
-    this.ticketListener();
-    this.setHiddeDiv(true);
+    this.showCurrentClient();
   }
 
+  ionViewDidLoad() {
+    this.hideTicketDiv();
+  }
+
+
+  public makeTransaction(){
+
+    if( true ){
+      // this.addClientToList()
+      this.setHiddeDiv(false);
+      this.TicketDiv();
+    }
+
+    if("sms acti"){}
+    if("sms acti"){}
+    if("sms acti"){}
+
+  }
+
+
+  //------------------------------------------THIS IS THE FIREBASE FUNCTION SECTION----------------------------------------------//
+
+
+  public checkPayment(){
+    const dbRefObject = firebase.database().ref().child('Tickets/paymentCompleted');
+    dbRefObject.on('value' , snap =>  this.ticketObject.paymentCompleted =   snap.val()  );
+    return this.ticketObject.paymentCompleted.toString();
+  }
+
+  //This function w
+  public showCurrentClient(){
+    const dbRefObject = firebase.database().ref().child('Tickets/currentPosition');
+    dbRefObject.on('value' , snap =>  this.ticketObject.currentPosition =   snap.val()  );
+
+  }
+
+  //This function  will add 1 to the ticket queu and set new position for teh client
+  private addClientToList(){
+    const dbRefObject = firebase.database().ref().child('Tickets');
+    dbRefObject.set({currentPosition:this.ticketObject.currentPosition + 1 });
+  }
+
+  //------------------------------------------THIS IS THE Algorithm  SECTION----------------------------------------------//
+
+
+  //------------------------------------------THIS IS THE OBJECT  SECTION----------------------------------------------//
+  private ticketObject = {
+    'currentPosition': 0,
+    'userPosition': 0,
+    'paymentCompleted':''
+  }
+
+  //------------------------------------------THIS IS THE HELPER FUNCTION SECTION----------------------------------------------//
   setHiddeDiv(value: any) {
     this.hiddenDiv = value;
   }
@@ -37,46 +85,26 @@ export class UserProfilePage {
     this.newAlert.presentAlert();
   }
 
-
-  private ticketListener(){
-    const dbRefObject = firebase.database().ref().child('Tickets/currentPosition');
-     dbRefObject.on('value' , snap =>  this.ticketObject.position =   snap.val()  );
-  }
-
-  private addTicketListener(){
-    const dbRefObject = firebase.database().ref().child('Tickets');
-    dbRefObject.set({currentPosition:this.ticketObject.position +1 });
-  }
-
-  public makeTransaction(){
-
-    if( this.checkPayment() == 'yes' ){
-      this.setHiddeDiv(false);
-      console.log("transaction completed");
-
-    }
-
-    if("sms acti"){}
-    if("sms acti"){}
-    if("sms acti"){}
-
-  }
-
-
-  public checkPayment(){
-    const dbRefObject = firebase.database().ref().child('Tickets/paymentCompleted');
-    dbRefObject.on('value' , snap =>  this.ticketObject.paymentCompleted =   snap.val()  );
-    return this.ticketObject.paymentCompleted.toString();
-  }
-
-  private ticketObject = {
-    'position': 0,
-    'paymentCompleted':''
-  }
-
   // Open ticket confirmation view page
   public getTicketConfirmation() {
     this.nav.push(TicketConfirmationPage);
+  }
+
+  TicketDiv(){
+    if(this.hiddenDiv == true){
+      this.hideTicketDiv();
+    }
+    else
+      this.showTicketDiv();
+  }
+
+  showTicketDiv(){
+    $('#ticketPosition').show();
+    $("#ticketPosition").delay(5000).hide(0);
+  }
+
+  hideTicketDiv(){
+    $('#ticketPosition').hide();
   }
 
 }
