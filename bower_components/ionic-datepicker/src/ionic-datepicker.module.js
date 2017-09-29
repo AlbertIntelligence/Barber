3db889236207654f57e5211d7d9422e080c91ce2
kodeu <koueni-anael.deumeni@polymtl.ca>
2017-09-29 04:51:40 -0400

    Stripe API installed.

--- bower_components/ionic-datepicker/src/ionic-datepicker.module.js
+++
@@ -1,8 +0,0 @@
-angular.module('ionic-datepicker', [
-  'ionic',
-  'ionic-datepicker.service',
-  'ionic-datepicker.provider',
-  'ionic-datepicker.templates',
-]);
-
-angular.module("myApp", ["ionic", "ion-datetime-picker"]);
