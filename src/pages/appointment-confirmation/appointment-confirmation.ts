5f1287f41a405372935cc049a735a71def6cde97
kodeu <koueni-anael.deumeni@polymtl.ca>
2017-10-09 01:50:13 -0400

    Appointment archive population.

--- src/pages/appointment-confirmation/appointment-confirmation.ts
+++ src/pages/appointment-confirmation/appointment-confirmation.ts
@@ -14,11 +14,21 @@ export class AppointmentConfirmationPage {
 
   private date:String;
   private heure:String;
+  private titleConfirmation:String;
+  private confirmationMessage:String;
 
   constructor(private navController: NavController, private navParams: NavParams)
   {
     this.date = navParams.get('date');
     this.heure = navParams.get('heure');
+
+    if (this.date == 'Cancellation') {
+      this.titleConfirmation = 'Annulation complétée !';
+      this.confirmationMessage = 'Votre rendez-vous a été annulé.'
+    } else {
+      this.titleConfirmation = 'Réservation complétée !';
+      this.confirmationMessage = 'Votre rendez-vous est le ' + this.date + 'à ' + this.heure + '.';
+    }
   }
 
 }
